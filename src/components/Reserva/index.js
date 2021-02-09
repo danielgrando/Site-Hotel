db6dd04f9dc4c7b8210d32e7fde6e064f4131660
Daniel <grando_daniel@hotmail.com>
2021-02-08 21:10:22 -0300

    commit

--- src/components/Reserva/index.js
+++ src/components/Reserva/index.js
@@ -1,8 +1,6 @@
 const menuToggle = document.querySelector('.toggle');
 const showcase = document.querySelector('.showcase');
 const inputs = document.querySelector('.inputs');
-const card = document.querySelector('.card');
-const hoteis = document.querySelector('.hoteis');
 
 menuToggle.addEventListener('click', () => {
   menuToggle.classList.toggle('active');
